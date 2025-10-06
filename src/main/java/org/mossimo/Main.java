fad98b8954436c69d7b3da6e7631ff6b43e7c71f
2585451 <2585451@edu.vaniercollege.qc.ca>
2025-10-06 09:12:00 -0400

    new info

--- src/main/java/org/mossimo/Main.java
+++ src/main/java/org/mossimo/Main.java
@@ -4,6 +4,33 @@ package org.mossimo;
 public class Main {
     // method
     public static void main(String[] args) {
+        // Student student;
+        /*
+        creating Student object that holds all
+        information from Student file
+         */
+
+        Student s1 = new Student(); // default constructor
+        /*
+        Same structure as:
+
+        Random random = new Random();
+        Scanner sc = new Scanner(System.in);
+         */
+
+        Student s2 = new Student();
+        Student s3 = new Student();
+        Student s4 = new Student();
+
+        System.out.println(s1);
+        /*
+        When printing, we get this: org.mossimo.Student@6acbcfc0
+
+        @6acbcfc0
+        This is the hexadecimal representation of the object's hash code
+        (a number that represents the object's memory address)
+         */
+
 
         }
     }
--- src/main/java/org/mossimo/Student.java
+++ src/main/java/org/mossimo/Student.java
@@ -3,7 +3,31 @@ package org.mossimo;
 public class Student {      // blueprint for creating a new data class
 
     // data member (field)
-    String name;
+    private String name;
+    private String gender;
+    private int age;
 
-    // method
+    // Constructor method (build a new object)
+    /*
+    Used to build an object
+        1. Always public
+        2. Always non-static (no static)
+        3. Always no return type (no void)
+        4. The name MUST be the same as the class name (Student)
+     */
+
+    // Default constructor
+    public Student() {
+        this.name = "Mike"; // this student's name
+        this.gender = "male"; // this student's gender
+        this.age = 18; // this student's age
+        /*
+        The 'this.' keyword is a reference to the current object
+        within an instance method or a constructor
+         */
+
+
+    }
+
+
 }
